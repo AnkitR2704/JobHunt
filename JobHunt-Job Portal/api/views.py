0de786c7e7fe74f81cd2693166acc49f0ce4d154
logan <ankitvk2704@gmail.com>
2025-10-21 14:55:27 +0530

    first version

---
+++ JobHunt-Job Portal/api/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .serializer import UserSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK,HTTP_400_BAD_REQUEST
+from rest_framework.decorators import api_view
+# Create your views here.
+
+def home(request):
+    return HttpResponse('Home page for api')
+
+@api_view(['GET','POST'])
+def registeruser(request):
+    if request.method == 'POST':
+        serializer_obj=UserSerializer(data=request.data)
+        if serializer_obj.is_valid() == True:
+            uobj=serializer_obj.save()
+            uobj.set_password(serializer_obj.validated_data['password'])
+            uobj.save()
+            Token.objects.create(user=uobj)
+            return Response(status=HTTP_200_OK)
+        else:
+            return Response(status=HTTP_400_BAD_REQUEST)
+            
+    
